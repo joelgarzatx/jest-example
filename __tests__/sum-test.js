3fa3eec9594241e186561488ad9c95ee136f183c
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-07-07 11:11:32 -0500

    use babel

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -1,9 +1,12 @@
 // unmock to use the actual implementation of sum
 jest.unmock('../sum');
 
+import sum from '../sum';
+
 describe('sum', () => {
+
   it('adds 1 + 2 to equal 3', () => {
-    const sum = require('../sum');
     expect(sum(1, 2)).toBe(3);
   });
+
 });
